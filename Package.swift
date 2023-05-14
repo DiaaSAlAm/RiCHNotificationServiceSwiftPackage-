8e763feff381f9d8d278002509101c3aa7dbd420
Diaa Salam <d.salam@t2.sa>
2023-05-15 00:54:00 +0300

    first commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RichSwiftPackage",
+    products: [
+        .library(
+            name: "RichSwiftPackage",
+            targets: ["RichSwiftPackage"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/arturdev/Unrealm.git", from: "1.9.5"),
+    ],
+    targets: [
+        .target(
+            name: "RichSwiftPackage",
+            dependencies: ["RiCHNotificationService", "Unrealm"]),
+        .binaryTarget(
+            name: "RiCHNotificationService",
+            path: "./RiCHNotificationService.xcframework"),
+    ]
+)
